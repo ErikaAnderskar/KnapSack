2df9a46ee7740ba33d59131c2dadabcdfbd1a1cd
Erika Anderskär <eanderskar@gmail.com>
2017-10-07 13:00:46 +0200

    greedy

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -1,4 +1,4 @@
-#' greedy_knapsack function
+#' greedy_knapsack_c function
 #' 
 #' @param x A data.frame with two variables v (values) and w (weights)
 #' @param W The maximum knapsack size
@@ -7,35 +7,60 @@
 #' @export
 
 
-greedy_knapsack <- function(x,W){
-if (W<0) stop ("W has to be a positive number")
+greedy_knapsack <- function(x,W,fast=FALSE){
+  if (W<0) stop ("W has to be a positive number")
+  x[,3]<- x[,1]/x[,2]
+  x$item <- 1:nrow(x)
+  x<- x[order(x$V3),]
+  
+  if (fast==TRUE){
+    item <- x[,4]
+    weight <-x[,1]
+    
+    
+    cppFunction('NumericVector iteminknapsack(NumericVector item, NumericVector weight, int W){
+                
+                int n=item.size();
+                NumericVector out(n);
+                double total = 0;
+                for ( int i=0; i < n; ++i){
+                total += weight[i];
+                if (total < W){
+                out[i] = item[i];
+                }
+                }
+                
+                return out;
+                
+  }')
 
-x[,3]<- x[,1]/x[,2]
-
-
-x$item <- 1:nrow(x)
-x<- x[order(x$V3),]
-
-
-result <- data.frame(weight=0,value=0,density=0,item=0)
-
-
-totalweight<-0
-
-for (i in 1:nrow(x)){
-totalweight <- x[i,1]+totalweight
-if(totalweight<W){
-  result[i,] <- x[i,]
-} 
-
+    
+    
+    elements <- iteminknapsack(item,weight,W)
+    value <- sum(x[elements==x[,4],2])
+    elements <- elements[elements!=0]
+    
+    
+    
+    return(list(value=value,elements=elements))
+} else {
+  
+  result <- data.frame(weight=0,value=0,density=0,item=0)
+  
+  
+  totalweight<-0
+  
+  for (i in 1:nrow(x)){
+    totalweight <- x[i,1]+totalweight
+    if(totalweight<W){
+      result[i,] <- x[i,]
+    } 
+    
+  }
+  
+  return(list(value=round(sum(result$value)), elements=result$item))
+  
 }
-
-return(list(value=round(sum(result$value)), elements=result$item))
-
-}
-
-
-
-
+  }
 
 
--- R/greedy_knapsack_c.R
+++ R/greedy_knapsack_c.R
@@ -7,14 +7,15 @@
 #' @export
 
 
-greedy_knapsack_C <- function(x,W){
+greedy_knapsack <- function(x,W,fast=FALSE){
   if (W<0) stop ("W has to be a positive number")
   x[,3]<- x[,1]/x[,2]
   x$item <- 1:nrow(x)
   x<- x[order(x$V3),]
 
-item <- x[,4]
-weight <-x[,1]
+  if (fast==TRUE){
+ item <- x[,4]
+ weight <-x[,1]
 
 
 cppFunction('NumericVector iteminknapsack(NumericVector item, NumericVector weight, int W){
@@ -40,9 +41,26 @@ value <- sum(x[elements==x[,4],2])
 elements <- elements[elements!=0]
 
 
-return(list(value=value,elements=elements))
-  
-}
 
+return(list(value=value,elements=elements))
+  } else {
+  
+    result <- data.frame(weight=0,value=0,density=0,item=0)
+    
+    
+    totalweight<-0
+    
+    for (i in 1:nrow(x)){
+      totalweight <- x[i,1]+totalweight
+      if(totalweight<W){
+        result[i,] <- x[i,]
+      } 
+      
+    }
+    
+    return(list(value=round(sum(result$value)), elements=result$item))
+    
+}
+}
 
 
